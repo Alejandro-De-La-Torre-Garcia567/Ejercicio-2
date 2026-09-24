45c1513d7eb9e7d161fcf9633b1c9c6709594664
Alejandro-De-La-Torre-Garcia567 <id00931782@usal.es>
2026-09-24 16:29:22 +0200

    implementacion console

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,16 +1,41 @@
+import java.io.Console;
+
 public class Ejercicio2 {
 
     public static void main(String[] args){
 
 
+           Console console=System.console();
+
+            if(console==null){
+
+                System.out.println("No hay una consola dispnible");
+
+            }  
+            
+            
+            int age,byear,ayear;
+            String b,r;
 
 
+            b=console.readline("Ingrese su año de nacimiento: ");
 
+            byear=Integer.parseint(b);
 
+            r=console.readline("Ingrese el año actual: ");
 
+            ryear=Integer.parseint(r);
 
+            
+            if(byear>ryear){
 
+                System.out.println("El año de nacimiento no puede ser mayor al año actual");
+            }
 
+            if(byear <0 || ryear<o){
+
+                System.out
+            }
     }
 
 
